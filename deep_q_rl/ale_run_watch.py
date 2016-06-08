625afed74fea115fc551a13e4213f39bd1f94a15
yzsatneychust <yzsatneychust@sina.com>
2016-06-08 10:35:04 +0800

    add network selection when watching the network play. otherwise would use params trained by nips to run nature network.

--- deep_q_rl/ale_run_watch.py
+++ deep_q_rl/ale_run_watch.py
@@ -9,12 +9,20 @@ import subprocess
 import sys
 
 def run_watch():
-    command = ['./run_nature.py', '--steps-per-epoch', '0',
-               '--test-length', '10000', '--nn-file', sys.argv[1],
+    if sys.argv[1] == 'nips':
+        script = './run_nips.py'
+    elif sys.argv[1] == 'nature':
+        script = './run_nature.py'
+    else:
+        print 'Usage: python ale_run_watch.py NETWORK_THAT_YOU_TRAINED(nips/nature) NETWORK_PKL_FILE [ ROM ]'
+        exit(0)  
+
+    command = [script, '--steps-per-epoch', '0',
+               '--test-length', '10000', '--nn-file', sys.argv[2],
                '--display-screen']
 
-    if len(sys.argv) > 2:
-        command.extend(['--rom', sys.argv[2]])
+    if len(sys.argv) > 3:
+        command.extend(['--rom', sys.argv[3]])
 
     p1 = subprocess.Popen(command)
     
